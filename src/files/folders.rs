d59fd698c71edeb2bf81c6e37447cb943b5de62a
Abhay Raj Singh <abhayonlyone@gmail.com>
2024-04-14 05:52:15 +0530

    fix: messages fix

--- src/files/folders.rs
+++ src/files/folders.rs
@@ -2,15 +2,15 @@ use std::path::PathBuf;
 
 pub fn checkNewProjectFolder(folder: PathBuf) -> Result<(), &'static str> {
     if !folder.is_dir() {
-        return Err("Folder is not a directory");
+        return Err("Not a directory.");
     }
 
     if let Ok(dir_contents) = folder.read_dir() {
         if dir_contents.peekable().peek().is_some() {
-            return Err("Directory not empty can't create a project");
+            return Err("Non empty directory.");
         }
     } else {
-        return Err("Error reading directory");
+        return Err("Error reading directory.");
     }
 
     Ok(())
