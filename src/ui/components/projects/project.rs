3b726d04b6093f81b731ef1425146821c290e271
Abhay Raj Singh <abhayonlyone@gmail.com>
2024-04-17 00:36:31 +0530

    feat: expansion toggle

--- src/ui/components/projects/project.rs
+++ src/ui/components/projects/project.rs
@@ -8,30 +8,30 @@ use crate::ui::components::projects::project_contents::ProjectContents;
 pub struct ProjectProps {
     name: String,
     location: String,
-    active: bool,
-    on_click: EventHandler<MouseEvent>,
 }
 pub fn Project(project: ProjectProps) -> Element {
+    let mut is_expanded = use_signal(|| false);
+
     rsx!(
         li {
-            class: "flex flex-col px-4",
-            onclick: move |event| project.on_click.call(event),
+            class: "flex flex-col pl-4 pr-2 group",
+            onclick: move |_| is_expanded.set(!is_expanded()),
             div {
                 class: "tooltip tooltip-right w-full text-left flex justify-between",
                 "data-tip": "{project.location}",
                 span {
-                    if project.active {
+                    if is_expanded() {
                         dioxus_free_icons::Icon { class: "inline", icon: FiChevronDown }
                     } else {
                         dioxus_free_icons::Icon { class: "inline", icon: FiChevronRight }
                     }
                     span { "{project.name}" }
                 }
-                button { class: "btn btn-sm btn-ghost btn-circle",
+                button { class: "btn btn-sm btn-ghost btn-circle opacity-0 group-hover:opacity-100",
                     dioxus_free_icons::Icon { class: "inline", icon: IoEllipsisVertical }
                 }
             }
-            div { class: format!("overflow-hidden {}", if project.active { "" } else { "max-h-0" }),
+            div { class: format!("overflow-hidden {}", if is_expanded() { "" } else { "max-h-0" }),
                 ProjectContents { location: &project.location.clone() }
             }
         }
--- src/ui/components/projects/projects_list.rs
+++ src/ui/components/projects/projects_list.rs
@@ -14,22 +14,10 @@ pub fn ProjectsList() -> Element {
             location: project.location.clone(),
         })
         .collect::<Vec<AamrsProject>>();
-    let mut activeProject = use_signal(|| Some(projects.first()?.name.clone()));
     return rsx!(
         ul { class: "flex flex-col space-y-3",
             for project in projects {
-                Project {
-                    name: project.name.clone(),
-                    location: project.location.clone(),
-                    active: activeProject().is_some() && project.name.eq(&activeProject().unwrap()),
-                    on_click: move |_| {
-                        if activeProject().is_none() {
-                            activeProject.set(Some(project.name.clone()))
-                        } else {
-                            activeProject.set(None)
-                        }
-                    }
-                }
+                Project { name: project.name.clone(), location: project.location.clone() }
             }
         }
     );
