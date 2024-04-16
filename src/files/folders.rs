4b4d11b6bb34038a269b52158111b3539dbccb42
Abhay Raj Singh <abhayonlyone@gmail.com>
2024-04-17 01:06:45 +0530

    feat: check directory tests

--- src/files/folders.rs
+++ src/files/folders.rs
@@ -12,3 +12,53 @@ pub fn checkNewProjectDirectory(directory: &Path) -> Result<(), String> {
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{fs, path::{Path, PathBuf}};
+
+    use anyhow::Result;
+    use tempfile::tempdir;
+
+    use super::checkNewProjectDirectory;
+
+    #[test]
+    fn empty_dir_is_valid()-> Result<()> {
+        let empty_dir = tempdir()?;
+
+        let check_result = checkNewProjectDirectory(empty_dir.path());
+        
+        assert!(check_result.is_ok());
+
+        Ok(())
+    }
+
+    #[test]
+    fn non_empty_dir_is_invalid()-> Result<()> {
+        let dir = tempdir()?;
+
+        let file_path = dir.path().join("my-temporary-note.txt");
+        fs::write(file_path, "Brian was here. Briefly.")?;
+
+        let check_result = checkNewProjectDirectory(dir.path());
+        
+        assert!(check_result.is_err());
+
+        Ok(())
+    }
+
+    #[test]
+    fn non_existant_dir_is_invalid() ->Result<()> {
+        let dir = tempdir()?;
+
+        let dir_path = PathBuf::from(dir.path());
+
+        let _ = dir.close();
+
+        let check_result = checkNewProjectDirectory(&dir_path);
+        
+        assert!(check_result.is_err());
+
+        Ok(())
+    }
+}
