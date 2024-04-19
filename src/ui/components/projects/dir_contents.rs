b1717129b66dcbec90473fe364b2790db7379da6
Abhay Raj Singh <abhayonlyone@gmail.com>
2024-04-19 07:33:21 +0530

    feat: directory rendering logic

--- src/ui/components/projects/dir_contents.rs
+++ src/ui/components/projects/dir_contents.rs
@@ -2,57 +2,138 @@ use std::path::PathBuf;
 
 use dioxus::prelude::*;
 use dioxus_free_icons::{
-    icons::fi_icons::{FiFile, FiFolder},
+    icons::{
+        fi_icons::{FiFile, FiFolder, FiFolderPlus, FiTrash},
+        io_icons::IoEllipsisVertical,
+    },
     Icon,
 };
 
 use crate::files::folder_contents::{get_folder_contents, Entry};
-#[derive(Props, Clone, PartialEq)]
-pub struct ProjectContentsProps {
-    directory: String,
+
+#[component]
+fn ContextMenu(path: Signal<String>, context_menu_open: Signal<bool>) -> Element {
+    rsx!(
+        ul {
+            // context menu
+            class: format!(
+                "absolute top-0 left-full menu bg-base-300 w-56 rounded-box {}",
+                if context_menu_open() { "" } else { "hidden" },
+            ),
+            li {
+                button {
+                    onclick: move |event| {
+                        event.stop_propagation();
+                        context_menu_open.set(false);
+                        println!("location: {}", path());
+                    },
+                    Icon { icon: FiFolderPlus }
+                    "Create folder"
+                }
+                button {
+                    onclick: move |event| {
+                        event.stop_propagation();
+                        context_menu_open.set(false);
+                        println!("location: {}", path());
+                    },
+                    Icon { icon: FiTrash }
+                    "Remove Project"
+                }
+            }
+        }
+    )
 }
-pub fn DirContents(props: ProjectContentsProps) -> Element {
-    let entries = get_folder_contents(&PathBuf::from(props.directory));
 
-    if entries.is_err() {
-        return rsx!( span {
-            class: "text-errror",
-            "Error reading directory",
-        });
-    }
-
-    let entries_rendered = entries
-        .unwrap()
-        .into_iter()
-        .map(|entry| match entry {
-            Entry::Dir(dir) => rsx!(
+#[component]
+fn DirRoot(name: String, path: Signal<String>) -> Element {
+    let mut context_menu_open = use_signal(|| false);
+    rsx! {
+        div {
+            class: "flex justify-between w-full group relative",
+            div {
+                class: "space-x-2",
                 Icon {
                     class: "inline",
                     icon: FiFolder
                 }
-                span { "{dir.name}"}
-            ),
-            Entry::File(file) => rsx!(
+                span { "{name}" },
+            }
+            button {
+                class: "btn btn-sm btn-ghost btn-circle opacity-0 group-hover:opacity-100",
+                onclick: move |event| {
+                    event.stop_propagation();
+                    context_menu_open.set(!context_menu_open());
+                },
+                dioxus_free_icons::Icon { class: "inline", icon: IoEllipsisVertical }
+            }
+            ContextMenu {path, context_menu_open}
+        }
+    }
+}
+
+#[derive(Props, Clone, PartialEq)]
+pub struct ProjectContentsProps {
+    name: String,
+    directory: PathBuf,
+    #[props(default = true)]
+    render_root: bool,
+    is_expanded: Option<Signal<bool>>,
+}
+
+pub fn Directory(props: ProjectContentsProps) -> Element {
+    let mut is_expanded = props.is_expanded.unwrap_or(use_signal(|| false));
+
+    let path = use_signal(|| props.directory.to_str().unwrap_or("<None>").to_string());
+
+    rsx! {
+        div{
+            class: "space-y-2",
+            onclick: move |event| {
+                event.stop_propagation();
+                is_expanded.set(!is_expanded());
+            },
+            if props.render_root {
+                DirRoot {name: props.name.clone(), path}
+            }
+            if is_expanded() {
+                Entries {directory: props.directory}
+            }
+        }
+    }
+}
+
+#[component]
+fn Entries(directory: PathBuf) -> Element {
+    let entries = get_folder_contents(&directory);
+
+    if entries.is_err() {
+        return rsx!( span {
+            class: "text-error",
+            "Error reading directory",
+        });
+    }
+
+    let entries_rendered = entries.unwrap().into_iter().map(|entry| match entry {
+        Entry::Dir(dir) => rsx!(Directory {
+            directory: dir.path,
+            name: dir.name.clone()
+        }),
+        Entry::File(file) => rsx!(
+            div {
+                // row
+                class: "space-x-2",
                 Icon {
                     class: "inline",
                     icon: FiFile
                 }
                 span { "{file.name}"}
-            ),
-        })
-        .map(|component| {
-            rsx!(
-                div {
-                    // row
-                    class: "space-x-2",
-                    {component}
-                }
-            )
-        });
+            }
+        ),
+    });
 
     rsx! {
-        div{
-            class: "p-4 bg-base-100 space-y-3",
+        div {
+            class: "pl-2 space-y-2",
             {entries_rendered}
         }
     }
--- src/ui/components/projects/project.rs
+++ src/ui/components/projects/project.rs
@@ -1,76 +1,37 @@
-use crate::ui::components::projects::dir_contents::DirContents;
+use std::path::PathBuf;
+
+use crate::ui::components::projects::dir_contents::Directory;
 use dioxus::prelude::*;
-use dioxus_free_icons::{
-    icons::{
-        fi_icons::{FiChevronDown, FiChevronRight, FiFolderPlus, FiTrash},
-        io_icons::IoEllipsisVertical,
-    },
-    Icon,
-};
+use dioxus_free_icons::{icons::fi_icons::FiChevronDown, Icon};
 #[derive(Props, Clone, PartialEq)]
 pub struct ProjectProps {
     name: String,
     location: String,
 }
 pub fn Project(project: ProjectProps) -> Element {
-    let mut is_expanded = use_signal(|| false);
-    let mut context_menu_open = use_signal(|| false);
-
     let project_location = use_signal(|| project.location);
+    let mut is_expanded = use_signal(|| false);
 
     rsx!(
         li {
             class: "flex flex-col pl-4 pr-2 group relative",
-            onclick: move |_| is_expanded.set(!is_expanded()),
             div {
-                class: "tooltip tooltip-right w-full text-left flex justify-between",
-                "data-tip": "{project_location}",
-                span {
-                    if is_expanded() {
-                        dioxus_free_icons::Icon { class: "inline", icon: FiChevronDown }
-                    } else {
-                        dioxus_free_icons::Icon { class: "inline", icon: FiChevronRight }
-                    }
-                    span { "{project.name}" }
+                class: "space-x-2",
+                onclick: move |event| {
+                    event.stop_propagation();
+                    is_expanded.set(!is_expanded());
+                },
+                Icon {
+                    class: "inline",
+                    icon: FiChevronDown,
                 }
-                button {
-                    class: "btn btn-sm btn-ghost btn-circle opacity-0 group-hover:opacity-100",
-                    onclick: move |event| {
-                        event.stop_propagation();
-                        context_menu_open.set(!context_menu_open());
-                    },
-                    dioxus_free_icons::Icon { class: "inline", icon: IoEllipsisVertical }
-                }
+                "{project.name}",
             }
-            div { class: format!("overflow-hidden {}", if is_expanded() { "" } else { "max-h-0" }),
-                DirContents { directory: project_location()}
-            }
-            ul {
-                // context menu
-                class: format!(
-                    "absolute top-0 left-full menu bg-base-300 w-56 rounded-box {}",
-                    if context_menu_open() { "" } else { "hidden" },
-                ),
-                li {
-                    button {
-                        onclick: move |event| {
-                            event.stop_propagation();
-                            context_menu_open.set(false);
-                            println!("location: {}", project_location());
-                        },
-                        Icon { icon: FiFolderPlus }
-                        "Create folder"
-                    }
-                    button {
-                        onclick: move |event| {
-                            event.stop_propagation();
-                            context_menu_open.set(false);
-                            println!("location: {}", project_location());
-                        },
-                        Icon { icon: FiTrash }
-                        "Remove Project"
-                    }
-                }
+            Directory {
+                directory: PathBuf::from(project_location()),
+                name: project.name.clone(),
+                render_root: false,
+                is_expanded,
             }
         }
     )
